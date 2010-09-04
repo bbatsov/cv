238f7d705c1480253597ff6758a9510b63e896b9
Bozhidar Batsov <lordbad@gmail.com>
2010-09-05 01:13:25 +0300

    - a couple of updates

---
+++ auto/master.el
@@ -0,0 +1,14 @@
+(TeX-add-style-hook "master"
+ (lambda ()
+    (TeX-run-style-hooks
+     "graphicx"
+     "latex2e"
+     "europecv10"
+     "europecv"
+     "helvetica"
+     "narrow"
+     "experience"
+     "education"
+     "skills"
+     "additional")))
+
